e7bd93a23d90f0d2c4af06cb3eca52e272eb2e81
luxiaok <me@luxiaok.com>
2017-03-11 14:04:12 +0800

    增加IndexHandler

--- handler/index.py
+++ handler/index.py
@@ -2,3 +2,9 @@
 # -*- coding:utf-8 -*-
 # Powered By KStudio
 # 控制中心
+
+import tornado
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("This is SlatAdmin Index Page.")
