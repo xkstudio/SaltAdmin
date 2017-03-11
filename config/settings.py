012f047995a3b157e62c7058f94cb494f8dc2285
luxiaok <me@luxiaok.com>
2017-03-11 15:57:31 +0800

    增加Config

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Powered By KStudio
+
+settings = dict(
+    template_path = 'view',
+    static_path = 'static',
+    static_url_prefix = '/static/',
+    xsrf_cookies = False,
+    cookie_secret = "db884468559f4c432bf1c1775f3dc9da",
+    login_url = "/login",
+    debug = True,
+    autoreload = True
+)
