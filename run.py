7f241bc661cb7cedc3c1412b1cdc9df2b9f90939
luxiaok <me@luxiaok.com>
2017-03-11 22:35:28 +0800

    改进初始化参数导入方式

--- run.py
+++ run.py
@@ -3,11 +3,11 @@
 # Powered By XK-Studio
 
 from app import SaltAdmin
-from config import settings
-from handler import route
+from config.settings import settings
+from handler.route import urls
 
 if __name__ == "__main__":
     host = '0.0.0.0'
     port = 8888
-    app = SaltAdmin.App(host,port,route.urls,settings.settings)
+    app = SaltAdmin.App(host,port,urls,settings)
     app.run()
