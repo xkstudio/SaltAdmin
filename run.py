f7af718fe3bf2408d8a6790110c55423a21a1d47
luxiaok <me@luxiaok.com>
2017-04-03 21:39:46 +0800

    增加db配置

--- run.py
+++ run.py
@@ -4,10 +4,11 @@
 
 from app import SaltAdmin
 from config.settings import settings
+from config.settings import db as db_conf
 from handler.route import urls
 
 if __name__ == "__main__":
     host = '0.0.0.0'
     port = 8888
-    app = SaltAdmin.App(host,port,urls,settings)
+    app = SaltAdmin.App(host,port,urls,settings,db_conf)
     app.run()
