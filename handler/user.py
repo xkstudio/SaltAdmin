d7314d0578d5158e7fcf6fbc01e225fd0d8217c4
luxiaok <me@luxiaok.com>
2017-04-03 21:41:21 +0800

    增加数据模型的测试

--- handler/user.py
+++ handler/user.py
@@ -4,8 +4,16 @@
 # 用户管理
 
 from BaseHandler import BaseHandler
+from model.user import User
 
 class UserHandler(BaseHandler):
     def get(self):
+        data = self.db.query(User).all()
+        for i in data:
+            print "*"*10
+            print "UID: %s" % i.id
+            print "Username: %s" % i.username
+            print "Password: %s" % i.password
+            print "E-mail: %s" % i.email
         #self.write("This is SlatAdmin Host Page.")
         self.render('user/index.html', title="用户管理")
