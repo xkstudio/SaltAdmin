36a5cebbb21db697189b8b26728faff0b32e443f
luxiaok <me@luxiaok.com>
2017-04-03 22:10:07 +0800

    增加基础数据库应用

--- handler/user.py
+++ handler/user.py
@@ -9,11 +9,4 @@ from model.user import User
 class UserHandler(BaseHandler):
     def get(self):
         data = self.db.query(User).all()
-        for i in data:
-            print "*"*10
-            print "UID: %s" % i.id
-            print "Username: %s" % i.username
-            print "Password: %s" % i.password
-            print "E-mail: %s" % i.email
-        #self.write("This is SlatAdmin Host Page.")
-        self.render('user/index.html', title="用户管理")
+        self.render('user/index.html', title="用户管理",data=data)
