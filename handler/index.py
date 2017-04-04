190e3f14cd4a0e6976b6e41dd0cd984ed14b2a57
luxiaok <me@luxiaok.com>
2017-04-04 19:27:09 +0800

    增加SaltStack版本显示

--- handler/index.py
+++ handler/index.py
@@ -12,9 +12,15 @@ class IndexHandler(BaseHandler):
     # 获取系统信息
     def get_system_info(self):
         tornado_verion = tornado.version
+        try:
+            import salt.version
+            salt_version = salt.version.__version__
+        except:
+            salt_version = 0
         data = {
             'tornado': tornado_verion,
-            'saltadmin': self.app_version
+            'saltadmin': self.app_version,
+            'saltstack': salt_version
         }
         return data
 
