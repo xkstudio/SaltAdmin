8c5e448bdad31a568a4f0f19f28a9d8dccd9d7a3
luxiaok <me@luxiaok.com>
2017-04-04 19:01:17 +0800

    增加get_system_info方法

--- handler/index.py
+++ handler/index.py
@@ -3,10 +3,21 @@
 # Powered By KStudio
 # 控制中心
 
+import tornado
 from BaseHandler import BaseHandler
 from tornado.web import authenticated as Auth
 
 class IndexHandler(BaseHandler):
+
+    # 获取系统信息
+    def get_system_info(self):
+        tornado_verion = tornado.version
+        data = {
+            'tornaod': tornado_verion
+        }
+        return data
+
     @Auth
     def get(self):
-        self.render('index/index.html',title="Hello",foo='bar')
+        info = self.get_system_info()
+        self.render('index/index.html',title="Hello",info=info)
