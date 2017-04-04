0670725b06f384185e431700ea3beaaf7fc5e90c
luxiaok <me@luxiaok.com>
2017-04-04 18:53:41 +0800

    更新用户登录逻辑，取消登录密码明文传输

--- handler/user.py
+++ handler/user.py
@@ -31,7 +31,7 @@ class LoginHandler(BaseHandler):
         user = self.db.query(User).filter_by(username=username).first()
         if not user:
             return self.jsonReturn({'code': -2, 'msg': '用户名错误'})
-        if password != user.password:
+        if self.md5(password) != user.password:
             return self.jsonReturn({'code': -3, 'msg': '密码错误'})
         # 验证通过，创建会话
         self.create_session(user,remember)
