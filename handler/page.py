626aa57b7eaf6fbd743934e37079473e65a62dce
luxiaok <me@luxiaok.com>
2017-04-04 09:58:32 +0800

    增加空白模版页面

--- handler/page.py
+++ handler/page.py
@@ -4,6 +4,12 @@
 
 from BaseHandler import BaseHandler
 
+# 404页面
 class Page404Handler(BaseHandler):
     def get(self):
         self.render('page/404.html', title="404")
+
+# 空白页
+class BlankHandler(BaseHandler):
+    def get(self):
+        self.render('page/blank.html', title="Blank")
