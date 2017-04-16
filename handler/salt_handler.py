fc1445b392963474b457ee9d8269fa1806e87935
luxiaok <me@luxiaok.com>
2017-04-16 19:50:13 +0800

    Add SaltMaster Data Render

--- handler/salt_handler.py
+++ handler/salt_handler.py
@@ -3,12 +3,14 @@
 # Powered By KK Studio
 
 from BaseHandler import BaseHandler
+from model.models import SaltMaster
 
 
 # Salt-Master管理
 class MasterHandler(BaseHandler):
     def get(self):
-        self.render('salt/master.html')
+        data = self.db.query(SaltMaster).all()
+        self.render('salt/master.html',data=data)
 
 
 # Salt-Key管理
