5bc16d744e49371abab5f8fd144e845ade5209b1
luxiaok <me@luxiaok.com>
2017-04-22 11:45:49 +0800

    Improve a db argument

--- app/DB.py
+++ app/DB.py
@@ -10,7 +10,7 @@ class Database:
 
     def __init__(self,host='localhost',port=3306,db='mysql',user='root',passwd='',charset='utf-8'):
         db_uri = 'mysql+mysqldb://%s:%s@%s:%s/%s?charset=%s' % (user,passwd,host,port,db,charset)
-        self.session = self.create_session(db_uri,encoding=charset)
+        self.session = self.create_session(db_uri,charset)
 
 
     def create_session(self,db_uri,encoding='utf-8'):
