3b0e004bf7a2a6d6ae3040cb00530f32359def46
luxiaok <me@luxiaok.com>
2017-04-22 11:43:27 +0800

    Improve db wrapper

--- app/DB.py
+++ app/DB.py
@@ -9,21 +9,15 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 class Database:
 
     def __init__(self,host='localhost',port=3306,db='mysql',user='root',passwd='',charset='utf-8'):
-        self._host = host
-        self._port = port
-        self._db = db
-        self._user = user
-        self._passwd = passwd
-        self._charset = charset
-        self._db_url = self._gen_db_conf()
+        db_uri = 'mysql+mysqldb://%s:%s@%s:%s/%s?charset=%s' % (user,passwd,host,port,db,charset)
+        self.session = self.create_session(db_uri,encoding=charset)
 
 
-    def _gen_db_conf(self):
-        db_url = 'mysql+mysqldb://%s:%s@%s:%s/%s?charset=%s' % \
-                 (self._user,self._passwd,self._host,self._port,self._db,self._charset)
-        return db_url
-
-
-    def db_session(self):
-        engine = create_engine(self._db_url, encoding=self._charset, echo=False)
+    def create_session(self,db_uri,encoding='utf-8'):
+        engine = create_engine(db_uri, encoding=encoding, echo=False)
         return scoped_session(sessionmaker(bind=engine, autocommit=False))
+
+
+    def close(self):
+        if self.session:
+            self.session.remove()
