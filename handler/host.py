ac812ff3ef5466bbe91053d82e96ea936825ea0c
luxiaok <me@luxiaok.com>
2017-04-23 17:18:36 +0800

    Add Pagination for hosts

--- handler/host.py
+++ handler/host.py
@@ -23,7 +23,10 @@ def get_groups(db):
 class IndexHandler(BaseHandler):
     @Auth
     def get(self):
-        data = self.db.query(Host).all()
+        page = int(self.get_argument('page',1))
+        line = int(self.get_argument('line',100))
+        offset = (page-1)*line
+        data = self.db.query(Host).offset(offset).limit(line).all()
         groups = get_groups(self.db)
         status = {
             0: u'<span class="label label-default"> 未知 </span>',
